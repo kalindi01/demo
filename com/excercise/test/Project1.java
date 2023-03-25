20d32de257e3f525c5554b71302415b0e3caf8c9
kalindi <kalindi00911@gmail.com>
2023-03-25 18:50:19 -0400

    developed subtract features

--- com/excercise/test/Project1.java
+++ com/excercise/test/Project1.java
@@ -3,15 +3,18 @@ package com.excercise.test;
 public class Project1 {
 
 	public static void main(String[] args) {
-		
-		
 		System.out.println(addition(5,10));
+		System.out.println(subtract(5,10));
 		
 
 	}
 	
 	static int addition(int a, int b) {
 		return a+b;
+	}
+	
+	static int subtract(int a, int b) {
+		return a-b;
 	}
 
 }
